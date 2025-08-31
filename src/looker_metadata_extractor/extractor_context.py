86f6b076501a0669a12e4f96f0bb2f9e0cd5564c
ClaireChilders <clairechilders@oakland.edu>
2025-08-30 22:24:01 -0400

    feat: add persistent browser context, add options
    remove browser property and instead use a context property, optionally loaded from persistent storage
    
    add `run_headless` and `reuse_context` for headless browser launch and persistent context respectively

--- src/looker_metadata_extractor/extractor_context.py
+++ src/looker_metadata_extractor/extractor_context.py
@@ -1,6 +1,7 @@
-from .auth.authenticator import Authenticator
-from .logger import logger
-from playwright.sync_api import sync_playwright, BrowserContext, Browser, Page
+from __future__ import annotations
+from looker_metadata_extractor.auth.auth_handler import AuthHandler
+from looker_metadata_extractor.logger import logger
+from playwright.sync_api import sync_playwright, BrowserContext, Page
 from typing import Callable
 
 class ExtractorContext:
@@ -8,31 +9,38 @@ class ExtractorContext:
     Holds the context for the extractor, including authentication and browser instance
     """
 
-    def __init__(self, auth: Authenticator, cookie_url: str, required_cookies: list[str]):
+    def __init__(self, auth: AuthHandler, cookie_url: str, required_cookies: list[str], run_headless: bool, reuse_context: bool):
         self.auth = auth
         self.cookie_url = cookie_url
         self.required_cookies = required_cookies
-        self.browser = None
         self.current_page = None
+        self.run_headless = run_headless
+        self.reuse_context = reuse_context
+        self.context = None
 
     def _get_context(self) -> BrowserContext | None:
-        if self.browser:
-            return self.browser.new_context()
+        if self.context:
+            return self.context
         return None
 
     def _new_context(self) -> BrowserContext:
-        if not self.browser:
+        if not self.context:
             logger.info("Starting new browser instance...")
-            self.browser = sync_playwright().start().chromium.launch(headless=True)
-        logger.info("Creating new browser context...")
-        return self.browser.new_context()
+            if self.reuse_context:
+                self.context = sync_playwright().start().chromium.launch_persistent_context(user_data_dir="./user_data", headless=self.run_headless)
+            else:
+                logger.info("Creating new browser context...")
+                browser = sync_playwright().start().chromium.launch(headless=self.run_headless)
+                self.context = browser.new_context()
+        return self.context
+
 
     def cleanup(self) -> None:
-        if self.browser:
+        if self.context:
             logger.info("Closing browser instance...")
-            self.browser.close()
-            self.browser = None
-    
+            self.context.close()
+            self.context = None
+
     def is_authenticated(self) -> bool:
         context = self._get_context()
         if context is None:
