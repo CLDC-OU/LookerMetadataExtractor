8ab3c602774ea10bb967e8fcddebaccb74afb3a1
ClaireChilders <clairechilders@oakland.edu>
2025-08-30 22:09:24 -0400

    feat: add NoAuthHandler for no auth

---
+++ src/looker_metadata_extractor/auth/no_auth_handler.py
@@ -0,0 +1,13 @@
+from looker_metadata_extractor.auth.auth_handler import AuthHandler
+
+class NoAuthHandler(AuthHandler):
+    """
+    No authentication handler that does nothing
+    Used when no authentication is required or when authentication is handled externally
+    """
+
+    def __init__(self, **kwargs):
+        pass
+
+    def authenticate(self, context):
+        pass
